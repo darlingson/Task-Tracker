b249129a3996618c8d0243e80c5ad16792d342db
darlingson <dmakuwila@gmail.com>
2024-03-01 21:12:47 +0200

    added my name as a greeting to test out display content from a variable

--- App.js
+++ App.js
@@ -2,9 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const name = "Darlingson";
   return (
     <View style={{height:100, width:100, backgroundColor: 'orange',}}>
       <Text>Hello World</Text>
+      <Text>{name}</Text>
+      <Text>Welcome to react native</Text>
+      <StatusBar style="auto" />
     </View>
   );
 }
