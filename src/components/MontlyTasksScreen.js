14b3e7ed8bd6f242df84e7c2d31e7a2216290b9b
darlingson <dmakuwila@gmail.com>
2024-03-11 21:19:09 +0200

    added code to display the task list in the monthlytaskscreen

--- src/components/MontlyTasksScreen.js
+++ src/components/MontlyTasksScreen.js
@@ -1,9 +1,16 @@
 import { Text, View } from "react-native"
+import { useList } from "../context/ListContext";
 
 const MonthlyTasksScreen = () => {
+    const {list} = useList();
     return (
         <View>
             <Text>This Month's Tasks</Text>
+            {list.map((task,index) => (
+                <View key={index}>
+                    <Text>{task.name}</Text>
+                </View>
+            ))}
         </View>
     )
 }
